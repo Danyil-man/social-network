0d40e8505d40251a7d720f93bfec1abffe468ce4
Danyil <demyanyuk76@gmail.com>
2021-11-11 18:39:29 +0200

    modal icon updated styles

---
+++ .history/src/ui/components/common/modal/Modal_20211111182221.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useState } from "react";
+import { modalState } from "../../../../core/store/redux/slice/modalSlica";
+//import { modalState } from "../../../../core/store/redux/slice/modalSlica";
+import m from "./Modal.module.scss"
+
+interface PropsModal {
+    closeModal: any;
+}
+
+const Modal:FC<PropsModal> = ({closeModal}) => {
+    const [isModal, setIsModal] = useState(true)
+    //const toggleModal = () => setIsModal(wasModal => !wasModal)
+    return( <>
+        {isModal && (
+        <div className={m.background}>
+        <div className={m.container}>
+            <div className={m.}>
+            <button className={m.titleCloseBtn} onClick={() => closeModal(false)}> X </button>
+            </div>
+        <div className={m.title}>
+         <h1>Upload a photo</h1>
+         </div>
+         <div className={m.body}>
+         <div>
+             <input 
+             //ref={refPickedRef} 
+             type="file" 
+             hidden 
+             //onChange={addImage} 
+             />
+         </div>
+         <div>
+             <input 
+             type="text"
+             //ref={caption}
+             placeholder="Enter description..."
+             />
+         </div>
+         </div>
+         <div className={m.footer}>
+             <button>
+                 Upload Photo
+             </button>
+         </div>
+        </div>
+     </div>
+     
+    )}
+    </>  
+    )
+}
+
+export default Modal;
