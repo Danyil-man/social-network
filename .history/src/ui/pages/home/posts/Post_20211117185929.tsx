e8810206f05b0b4b783cdb58d0acfb4928ffbd4d
Danyil <demyanyuk76@gmail.com>
2021-11-17 20:37:08 +0200

    Finaly finished styles for modalPost, fixed bugs with main icon(scrollbar), fixed bugs with header component, changed styles for adaptive content, now header fixed

---
+++ .history/src/ui/pages/home/posts/Post_20211117185929.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import PostModal from "../../../components/postModal/postModal";
+import post from "./Post.module.scss";
+
+interface PostData {
+    id: string;
+    username: string;
+    postAva: string;
+    postImg: string;
+    postDescription: string;
+    nav: string;
+    status: string;
+}
+
+
+
+const Post = (props: PostData) => {
+
+    function Share() {
+        alert('Share with ...')
+    }
+
+    function Edit() {
+        alert('Edit')
+    }
+
+    const [isModal, setIsModal] = useState(false)
+    const likeBlock = () => {
+        return (
+            <div className={post.likes}>
+                <i className="far fa-heart"></i>
+                <b>439</b>
+            </div>
+        )
+    }
+
+    return (
+        <div className={post.postItem}>
+            <div className={post.header}>
+                <div className={post.leftHeader}>
+                    <img width={40} src={props.postAva} alt="postHeader" />
+                    <div className={post.content_info}>
+                        <p className={post.username}> {props.username} </p>
+                        <p className={post.status}>{props.status}</p>
+                    </div>
+                </div>
+
+                <div className={post.rightHeader}>
+                    <button className={post.btnedit} onClick={Edit}>
+                        <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                </div>
+
+            </div>
+
+            <div className={post.mainImg}>
+                <img width={560} onClick={() => setIsModal(true)} src={props.postImg} alt="post" />
+            </div>
+
+            <div className={post.interaction}>
+                <div className={post.leftInteracion}>
+                    <div className={post.likes}>
+                        <i className="far fa-heart"></i>
+                        <b>439</b>
+                    </div>
+                    <div className={post.comments}>
+                        <i className="far fa-comment"></i>
+                        <b>34</b>
+                    </div>
+                </div>
+
+                <div className={post.rightIneraction}>
+                    <button onClick={Share} >Share<i className="sharearrow fas fa-arrow-right"></i></button>
+                </div>
+            </div>
+
+            <div className={post.text}>
+                <p>{props.postDescription}</p>
+            </div>
+
+            {isModal && (
+                <PostModal
+                    closeModal={setIsModal}
+                    postAva={props.postAva}
+                    username={props.username}
+                    postImg={props.postImg}
+                />
+            )}
+
+        </div>
+
+    )
+}
+
+export default Post;
