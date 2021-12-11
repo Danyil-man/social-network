147831c640f5af509503b4802f1ce8db51830446
Danyil <demyanyuk76@gmail.com>
2021-12-11 12:01:23 +0200

    took users photo From API

--- .history/src/ui/pages/home/stories/Story_20211211115947.tsx
+++ .history/src/ui/pages/home/stories/Story_20211211115947.tsx
@@ -1,7 +1,7 @@
 import { GetUserType } from "core/store/reducers/usersReducer";
 import React, { FC } from "react";
 import style from "./Stories.module.scss";
-import UserPhoto from 'public/images/withoutphoto.png'
+import UserPhoto from '../../../../public/images/withoutphoto.png'
 
 interface StoryProps {
     user: GetUserType
@@ -11,7 +11,7 @@ const Story: FC<StoryProps> = ({ user }) => {
     return (
         <div>
             <div className={style.item}>
-                <img width={64} src={user.profile_photo_url != null ? user.profile_photo_url : UserPhoto} alt="story" />
+                <img width={64} src={user.profile_photo_url != null ? user.profile_photo_url : } alt="story" />
                 <p>UserName{user.username} </p>
             </div>
         </div>
