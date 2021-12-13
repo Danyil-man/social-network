45bb71713eab531db350105eec77621ae00d9297
Danyil <demyanyuk76@gmail.com>
2021-12-13 13:10:51 +0200

    some changes in profile reducer

---
+++ .history/src/ui/pages/home/Home_20211213124303.tsx
@@ -0,0 +1,49 @@
+import React, { FC, useState } from "react";
+import Stories from "./stories/Stories";
+import style from "./Home.module.scss";
+import Posts from "./posts/Posts";
+import MiniProfile from "./miniProfile/MiniProfile";
+import { AccountType, GetAccountType } from "core/store/api/api";
+import { GetUserType } from "core/store/reducers/usersReducer";
+
+type Homeprops = {
+    profile?: GetAccountType
+    isLoading: boolean
+    users: Array<GetUserType>
+    userData: GetUserType
+    editProfile: (account: AccountType) => void
+    getUsers: () => void
+    getProfileUser: (username: string) => void
+}
+
+const Home: FC<Homeprops> = ({ profile, isLoading, users, editProfile, getUsers, getProfileUser }) => {
+
+    return (
+        <div className={style.wrapper}>
+            <div className={style.content}>
+                <div className={style.lefside}>
+                    <Stories
+                        users={users}
+                        getUsers={getUsers}
+                    />
+                    <Posts
+                        users={users}
+                        getProfileUser={getProfileUser}
+                        profile={profile}
+                        editProfile={editProfile}
+                        isLoading={isLoading}
+                    />
+                </div>
+                <div className={style.rightside}>
+                    <MiniProfile
+                        profile={profile}
+                        isLoading={isLoading}
+                        editProfile={editProfile}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
