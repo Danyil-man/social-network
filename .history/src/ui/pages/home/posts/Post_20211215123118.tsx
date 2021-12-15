d13e188541f688b980e62436bd2ab860dc71b4cc
Danyil <demyanyuk76@gmail.com>
2021-12-15 18:28:51 +0200

    added Like & Remove Like Functionality

---
+++ .history/src/ui/pages/home/posts/Post_20211215123118.tsx
@@ -0,0 +1,99 @@
+import { GetUserType } from "core/store/reducers/usersReducer";
+import React, { FC, useEffect, useState } from "react";
+import PostModal from "ui/components/postModal/postModal";
+import style from "./Post.module.scss";
+import UserPhoto from 'public/images/withoutphoto.png'
+import Profile from "ui/pages/profile/profileContent/Profile";
+import { AccountType, GetAccountType, GetAllPostsType } from "core/store/api/api";
+import { Link } from "react-router-dom";
+import Preloader from "ui/components/common/Preloader";
+
+
+interface PostType {
+    profile?: GetAccountType
+    isLoading: boolean
+    post: GetAllPostsType
+    editProfile: (account: AccountType) => void
+    getProfileUser: (username: string) => void
+}
+
+const Post: FC<PostType> = ({ post, isLoading, profile, getProfileUser, editProfile }) => {
+
+    function Share() {
+        alert('Share with ...')
+    }
+
+    function Edit() {
+        alert('Edit')
+    }
+    const username = post.author.username;
+    const TakeUser = () => {
+        getProfileUser(username)
+        console.log("Username:", username)
+    }
+
+    const [like, setLike] = useState(post.likes_count)
+
+    console.log('PostItem', post)
+    const [isModal, setIsModal] = useState(false)
+    return (
+        <div className={style.postItem}>
+            {isLoading ? <Preloader /> : null}
+            <div className={style.header}>
+                <div className={style.leftHeader}>
+                    <Link to={`/profile/${username}`}>
+                        <img onClick={TakeUser} width={40} src={post.author.profile_photo_url !== null ? post.author.profile_photo_url : UserPhoto} alt="postHeader" />
+                    </Link>
+                    <div className={style.content_info}>
+                        <p className={style.username}> {post.author.username} </p>
+                        <p className={style.status}>{post.created_at} </p>
+                    </div>
+                </div>
+
+                <div className={style.rightHeader}>
+                    <button className={style.btnedit} onClick={Edit}>
+                        <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                </div>
+
+            </div>
+
+            <div className={style.mainImg}>
+
+                <img key={post.photos.id} src={post.photos.url} width={560} onClick={() => setIsModal(true)} alt="post" />
+            </div>
+
+            <div className={style.interaction}>
+                <div className={style.leftInteracion}>
+                    <div className={style.likes}>
+                        <i onClick={() => like + 1} className="far fa-heart"></i>
+                        <b>{like}</b>
+                    </div>
+                    <div className={style.comments}>
+                        <i className="far fa-comment"></i>
+                        <b>{post.comments_count}</b>
+                    </div>
+                </div>
+
+                <div className={style.rightIneraction}>
+                    <button onClick={Share} >Share<i className="sharearrow fas fa-arrow-right"></i></button>
+                </div>
+            </div>
+
+            <div className={style.text}>
+                <p>{post.description}</p>
+            </div>
+
+            {isModal && (
+                <PostModal
+                    closeModal={setIsModal}
+
+                />
+            )}
+
+        </div>
+
+    )
+}
+
+export default Post;
---
+++ .history/src/ui/pages/profile/index_20211215114755.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useEffect, useState } from "react";
+import headerAva from "public/images/MiniProf/header__ava.png";
+import Profile from "./profileContent/Profile";
+import { connect } from "react-redux";
+import { AppStateType } from "core/store/redux/reduxStore";
+import { AccountType, GetAccountType, GetAllPostsType } from "core/store/api/api";
+import { Redirect } from "react-router";
+import { editProfile, getProfileUser } from "core/store/reducers/profileReducer";
+import { getIsLoading } from "core/store/selectors";
+
+type ContainerProfileType = {
+    isAuth: boolean
+    profile?: GetAccountType
+    posts: Array<GetAllPostsType>
+    editProfile: (account: AccountType) => void
+    isLoading: boolean
+    getProfileUser: (username: string) => void
+}
+
+const IdxProfile: FC<ContainerProfileType> = ({ isAuth, profile, posts, editProfile, getProfileUser, isLoading }) => {
+    let usernameProf = profile?.username
+    useEffect(() => {
+        if (!usernameProf) {
+            usernameProf = profile?.username
+        }
+        getProfileUser(usernameProf)
+    }, [])
+
+    return isAuth ? (
+        <div>
+            <Profile
+                profile={profile}
+                posts={posts}
+                editProfile={editProfile}
+                isLoading={isLoading}
+            />
+
+        </div>
+    ) : (
+        <Redirect to='/signup' />
+    )
+}
+
+const mapStateToprops = (state: AppStateType) => ({
+    isAuth: state.auth.isAuth,
+    profile: state.profile.profile,
+    posts: state.posts.posts,
+    isLoading: getIsLoading(state),
+})
+
+
+
+export default connect(mapStateToprops, { editProfile, getProfileUser })(IdxProfile);
